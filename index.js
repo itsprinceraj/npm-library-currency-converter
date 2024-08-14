2d81950504bbec6ee32daa75d83eda8a272ca70c
devops-prince <dev.prince116@gmailcom>
2024-08-15 01:46:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import Freecurrencyapi from "@everapi/freecurrencyapi-js";
 
+//  take instance of currency api
 const freecurrencyapi = new Freecurrencyapi(
   "fca_live_HxsNs5lDYeTxPgKL3BZGHETTTzIbnwHulin2w5Qy"
 );
 
+//  create an async function and make api call
 export async function convertCurrency(baseCurrency, toCurrency, unit) {
   const response = await freecurrencyapi.latest({
     base_currency: baseCurrency,
     currencies: toCurrency,
   });
 
+  //  fetch the value of converted currency
   const multiplier = response.data[toCurrency];
   console.log(multiplier);
-  return multiplier * unit;
+  return multiplier * unit; // return it
 }
 
 // convertCurrency("USD", "INR", 1);
